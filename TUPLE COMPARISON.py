5ce13e5a4ac86ff8d7b0b77a12bf69e3c9e1d4bf
KvnCncpcion <kvncncpcion@gmail.com>
2022-12-14 10:42:03 +0800

    Tuple Comparison Example

---
+++ TUPLE COMPARISON.py
@@ -0,0 +1,12 @@
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+per_comp1st = person1 < person2
+
+per_comp2nd = person2 < person3
+
+print("The result of the first comparison is:",per_comp1st)
+print("")
+
+print("The result of the second comparison is:",per_comp2nd)
