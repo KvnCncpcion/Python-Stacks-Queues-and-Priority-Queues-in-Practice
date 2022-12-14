10d3614f663863df3d51a57e212111cd164ab223
KvnCncpcion <kvncncpcion@gmail.com>
2022-12-14 10:21:04 +0800

    Heappush Example

---
+++ PRIOHEAP.py
@@ -0,0 +1,8 @@
+from heapq import heappush
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+print("This is the list of elements in the Queue:",fruits)
