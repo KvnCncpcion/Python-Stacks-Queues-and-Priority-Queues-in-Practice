de8a6ea095ff4befa018c1380243e6695020b7ed
KvnCncpcion <kvncncpcion@gmail.com>
2022-12-14 10:10:34 +0800

    Second LIFO Example

---
+++ LIFO2ND.py
@@ -0,0 +1,17 @@
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print("These are the elements in the Queue:",lifo)
+print("")
+
+print("The",lifo.pop(),"element has been Dequeued",lifo)
+print("")
+
+print("The",lifo.pop(),"element has been Dequeued",lifo)
+print("")
+
+print("The",lifo.pop(),"element has been Dequeued",lifo)
+print("")
