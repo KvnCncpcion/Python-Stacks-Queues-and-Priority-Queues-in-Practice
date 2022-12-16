71609ea7024ae5cae9e19ba81a4a16bee0ae7eaf
KvnCncpcion <kvncncpcion@gmail.com>
2022-12-16 18:50:26 +0800

    Priority Queue Original Example

--- PrioQueueOrigExample.py
+++ PrioQueueOrigExample.py
@@ -10,4 +10,4 @@ messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
 messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
 messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
 
-messages.dequeue()
+print(messages.dequeue())
